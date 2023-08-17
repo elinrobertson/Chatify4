80ca76f06c795ab922051d35d267cc5c13a1abc5
Sara Agnestrand <sara@agnestrand.net>
2023-08-17 15:45:46 +0200

    Co-authored-by: Alexandra Jernberg <aljer80@users.noreply.github.com> Co-authored-by: elinrobertson <elinrobertson@users.noreply.github.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,32 @@
+import { useEffect } from "react";
+import { io } from "socket.io-client";
+import "./Main.css"
+
+
 function App() {
+  const socket = io("http://localhost:3000/", { autoConnect: false} )
+
+  const initChat = () => {
+    socket.connect();
+    
+  }
+  
+ useEffect( () => {
+    socket.on("test", (data) =>{
+      console.log(data);
+    })
+ },[])
+  
   return (
-    <div>App</div>
+    <>
+   
+      <div className="login-container">
+      <h1>Chatify</h1>
+        <input type="text" placeholder="Skriv ditt namn här..."/>
+        <button onClick={initChat}>Börja chatta</button>
+      </div>
+    
+    </>
   )
 }
 
